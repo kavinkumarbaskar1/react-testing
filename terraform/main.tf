331b0483e0e8c687c9c893bf9b17ca73f3d8153f
kavinkumarbaskar1 <126063720+kavinkumarbaskar1@users.noreply.github.com>
2025-11-27 16:03:59 +0530

    Add Terraform configurations, bash automation scripts, and doc updates for Azure infrastructure provisioning.

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+# Terraform code for Azure Resources
+
+// Define Resource Group
+resource "azurerm_resource_group" "react_testing_rg" {
+   --------------- specialized submodules etc., ...
+}
+//...rest for compute monitoring!
